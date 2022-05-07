301a6c755818f483ad427dc2186b45e6e53c0df4
Oleg Korneev <waterpoleg@gmail.com>
2022-05-07 23:58:50 +0300

    add addItem func to index controller

--- src/main/java/ru/job4j/todo/controller/IndexController.java
+++ src/main/java/ru/job4j/todo/controller/IndexController.java
@@ -16,4 +16,9 @@ public class IndexController {
     public String index(Model model) {
         return "index";
     }
+
+    @GetMapping("/addItem")
+    public String addItem(Model model) {
+        return "addItem";
+    }
 }
