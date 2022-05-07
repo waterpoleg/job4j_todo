9a4d17e0b6d364ab0531ccc002ff8df3b19610f8
Oleg Korneev <waterpoleg@gmail.com>
2022-05-07 23:13:58 +0300

    add item model

--- src/main/java/ru/job4j/todo/model/Item.java
+++ src/main/java/ru/job4j/todo/model/Item.java
@@ -1,4 +1,59 @@
 package ru.job4j.todo.model;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Table(name = "items")
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private String created = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+    private boolean done;
+
+    public Item() {
+    }
+
+    public Item(int id, String description, String created, boolean done) {
+        this.id = id;
+        this.description = description;
+        this.created = created;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
