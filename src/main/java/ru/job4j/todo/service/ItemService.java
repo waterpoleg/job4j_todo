c82b9f84f6e1980f447363ea2c339ac02a463b64
Oleg Korneev <waterpoleg@gmail.com>
2022-05-07 23:39:33 +0300

    add item service

--- src/main/java/ru/job4j/todo/service/ItemService.java
+++ src/main/java/ru/job4j/todo/service/ItemService.java
@@ -1,4 +1,25 @@
 package ru.job4j.todo.service;
 
+import org.springframework.stereotype.Service;
+import ru.job4j.todo.model.Item;
+import ru.job4j.todo.persistence.ItemStore;
+
+import java.util.ArrayList;
+
+@Service
 public class ItemService {
+
+    private final ItemStore itemStore;
+
+    public ItemService(ItemStore itemStore) {
+        this.itemStore = itemStore;
+    }
+
+    public void create(Item item) {
+        itemStore.add(item);
+    }
+
+    public Object findAll() {
+        return new ArrayList<>(itemStore.findAll());
+    }
 }
