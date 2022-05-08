38476d18970f881cccc5dfcd94a5b17d30769826
Oleg Korneev <waterpoleg@gmail.com>
2022-05-08 23:10:13 +0300

    add list items mapping

--- src/main/java/ru/job4j/todo/controller/IndexController.java
+++ src/main/java/ru/job4j/todo/controller/IndexController.java
@@ -3,9 +3,16 @@ package ru.job4j.todo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.todo.service.ItemService;
 
 @Controller
 public class IndexController {
+
+    private final ItemService itemService;
+
+    public IndexController(ItemService itemService) {
+        this.itemService = itemService;
+    }
 
     @GetMapping("/")
     public String root() {
@@ -15,6 +22,12 @@ public class IndexController {
     @GetMapping("/index")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/items")
+    public String items(Model model) {
+        model.addAttribute("items", itemService.findAll());
+        return "items";
     }
 
     @GetMapping("/addItem")
