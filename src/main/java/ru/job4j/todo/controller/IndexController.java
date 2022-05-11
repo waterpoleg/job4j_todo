2661bd42db7d097158327e2ca93c81790ed1c8f8
Oleg Korneev <waterpoleg@gmail.com>
2022-05-11 14:02:11 +0300

    refactor: divide controllers

--- src/main/java/ru/job4j/todo/controller/IndexController.java
+++ src/main/java/ru/job4j/todo/controller/IndexController.java
@@ -4,18 +4,10 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.job4j.todo.model.Item;
-import ru.job4j.todo.service.ItemService;
 
 @ThreadSafe
 @Controller
 public class IndexController {
-
-    private final ItemService itemService;
-
-    public IndexController(ItemService itemService) {
-        this.itemService = itemService;
-    }
 
     @GetMapping("/")
     public String root() {
@@ -25,40 +17,5 @@ public class IndexController {
     @GetMapping("/index")
     public String index(Model model) {
         return "index";
-    }
-
-    @GetMapping("/items")
-    public String items(Model model) {
-        model.addAttribute("items", itemService.findAll());
-        return "items";
-    }
-
-    @GetMapping("/addItem")
-    public String addItem(Model model) {
-        return "addItem";
-    }
-
-    @PostMapping("/saveItem")
-    public String saveItem(@ModelAttribute Item item) {
-        itemService.create(item);
-        return "redirect:/items";
-    }
-
-    @GetMapping("/completedItems")
-    public String completedItems(Model model) {
-        model.addAttribute("items", itemService.findByCondition(true));
-        return "items";
-    }
-
-    @GetMapping("/newItems")
-    public String newItems(Model model) {
-        model.addAttribute("items", itemService.findByCondition(false));
-        return "items";
-    }
-
-    @GetMapping("/itemDetails/{itemId}")
-    public String itemDetails(Model model, @PathVariable("itemId") int id) {
-        model.addAttribute("item", itemService.findById(id));
-        return "itemDetails";
     }
 }
---
+++ src/main/java/ru/job4j/todo/controller/ItemController.java
@@ -0,0 +1,57 @@
+package ru.job4j.todo.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.todo.model.Item;
+import ru.job4j.todo.service.ItemService;
+
+@ThreadSafe
+@Controller
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/items")
+    public String items(Model model) {
+        model.addAttribute("items", itemService.findAll());
+        return "items";
+    }
+
+    @GetMapping("/addItem")
+    public String addItem(Model model) {
+        return "addItem";
+    }
+
+    @PostMapping("/saveItem")
+    public String saveItem(@ModelAttribute Item item) {
+        itemService.create(item);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/completedItems")
+    public String completedItems(Model model) {
+        model.addAttribute("items", itemService.findByCondition(true));
+        return "items";
+    }
+
+    @GetMapping("/newItems")
+    public String newItems(Model model) {
+        model.addAttribute("items", itemService.findByCondition(false));
+        return "items";
+    }
+
+    @GetMapping("/itemDetails/{itemId}")
+    public String itemDetails(Model model, @PathVariable("itemId") int id) {
+        model.addAttribute("item", itemService.findById(id));
+        return "itemDetails";
+    }
+}
