0d9058181076cd0e13d0767cf7ad5863b0736b18
Oleg Korneev <waterpoleg@gmail.com>
2022-05-11 22:55:56 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/todo/persistence/ItemStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemStore.java
@@ -3,11 +3,12 @@ package ru.job4j.todo.persistence;
 import net.jcip.annotations.ThreadSafe;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Item;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.function.Function;
 
 @ThreadSafe
 @Repository
@@ -19,71 +20,58 @@ public class ItemStore {
         this.sf = sf;
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     public void add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> {
+            session.save(item);
+            return item;
+        });
     }
 
     public Collection<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List items = session
-                .createQuery("from Item")
-                .list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
+        return this.tx(session -> session.createQuery("from Item ").list());
     }
 
     public Collection<Item> findByCondition(boolean condition) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List items = session
-                .createQuery("from Item where done = :param")
+        return this.tx(session -> session.createQuery("from Item where done = :param")
                 .setParameter("param", condition)
-                .list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
+                .list());
     }
 
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return this.tx(session -> session.get(Item.class, id));
     }
 
     public void setCompleted(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("update Item set done = true where id = :id")
+        this.tx(session -> session.createQuery("update Item set done = true where id = :id")
                 .setParameter("id", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+                .executeUpdate());
     }
 
     public void deleteItem(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("delete from Item where id = :id")
+        this.tx(session -> session.createQuery("delete from Item where id = :id")
                 .setParameter("id", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+                .executeUpdate());
     }
 
     public void updateItem(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> {
+            session.update(item);
+            return item;
+        });
     }
 }
